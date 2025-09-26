9bda6dea4fd60ef7db871cc436526efc15d3e3a6
Umer Munir <umermunir414@gmail.com>
2025-09-26 14:05:55 +0500

    Icon added in notification

--- server/utils/notifications.js
+++ server/utils/notifications.js
@@ -4,10 +4,36 @@ async function sendNotificationToUser(token, title, body) {
   if (!token) throw new Error("FCM token is required");
 
   //this will send firebase default notification
+  // const message = {
+  //   notification: { title, body },
+  //   token,
+  // };
+
   const message = {
-    notification: { title, body },
-    token,
-  };
+  token,
+  notification: { 
+    title, 
+    body, 
+    image: "https://lingua-link-zeta.vercel.app/icon.png" // 🔹 large image
+  },
+  android: {
+    notification: {
+      icon: "ic_notification", // small icon
+      imageUrl: "hhttps://lingua-link-zeta.vercel.app/icon.png",
+    },
+  },
+  apns: {
+    payload: {
+      aps: {
+        sound: "default",
+      },
+    },
+    fcm_options: {
+      image: "https://lingua-link-zeta.vercel.app/icon.png",
+    },
+  },
+};
+
 
 
   try {
